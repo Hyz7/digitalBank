4558c237fe1585aa73cd8f630a7b7d526a8f9b6a
769062519 <769062159.com>
2019-05-30 11:20:31 +0800

    update

--- js/index.js
+++ js/index.js
@@ -1,7 +1,8 @@
 $(function () {
-	$('self').hover(function () {
-        
+	$('.item').hover(function () {
+        // $(this).find('.submenu li').stop().fadeIn()
+
     },function () {
-        
+        // $(this).find('.submenu').fadeOut().stop()
     })
 })
